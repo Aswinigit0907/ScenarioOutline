c5de0245b692ed2c72d1fe81c236d3ed10740af0
Aswini S (MAS) <aswis@wipro.com>
2019-08-23 13:26:56 +0200

    First Commit

---
+++ src/test/java/Demo/AddCustomer.java
@@ -0,0 +1,50 @@
+package Demo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import junit.framework.Assert;
+
+public class AddCustomer {
+
+	static WebDriver driver;
+	@Given("User should launch browser")
+	public void user_should_launch_browser() {
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\aswis\\Documents\\Selenium\\chromedriver_win32 (1)\\chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.get("https://www.demo.guru99.com/telecom/");
+	}
+
+	@Given("User click add customer link")
+	public void user_click_add_customer_link(){
+		
+		driver.findElement(By.xpath("(//a[text()='Add Customer'])[1]")).click();  
+		driver.manage().window().maximize();
+	}
+
+	@When("user provide valid details")
+	    public void user_provide_valid_details() {
+		
+	driver.findElement(By.xpath("//label[text()='Done']")).click();
+	driver.findElement(By.id("fname")).sendKeys("Aswini");
+	driver.findElement(By.id("lname")).sendKeys("Subramani");
+	driver.findElement(By.id("email")).sendKeys("aswinijayalakshmi@gmail.com");
+	driver.findElement(By.xpath("//textarea[@name='addr']")).sendKeys("Chennai");
+	driver.findElement(By.id("telephoneno")).sendKeys("9344278231");
+	driver.findElement(By.xpath("//input[@name='submit']")).click();
+	}
+
+	@Then("to verify the customer id is displayed")
+
+	
+	   public void to_verify_the_customer_id_is_possible(){
+		
+	Assert.assertTrue((driver.findElement(By.xpath("//td[@align='center']")).isDisplayed()));
+	   
+	}
+
+
+}
---
+++ src/test/resources/Features/Try.feature
@@ -0,0 +1,10 @@
+
+Feature: Title of your feature
+  I want to use this template for my feature file
+
+  @tag1
+  Scenario: Generate the customer ID
+    Given User should launch browser
+    And User click add customer link
+    When user provide valid details
+    Then to verify the customer id is displayed
